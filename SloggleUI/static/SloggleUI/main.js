6cc09432e9823ea4d2a3b34f0df617220159db89
Suprava Jena <supravajena48@gmail.com>
2021-02-26 03:01:01 +0530

    UI modifications

--- SloggleUI/static/SloggleUI/main.js
+++ SloggleUI/static/SloggleUI/main.js
@@ -12,21 +12,25 @@ const signupForm = document.getElementById("signupForm");
 const hireFreelancerButton = document.getElementById("hireFreelancerButton");
 const beFreelancerButton = document.getElementById("beFreelancerButton");
 const signupCancelButton = document.getElementById("signupCancelButton");
+const makeYourChoice = document.getElementById("makeYourChoice");
 
 hireFreelancerButton.addEventListener("click", function () {
   signupForm.classList.remove("d-none");
   hireFreelancerButton.classList.add("d-none");
   beFreelancerButton.classList.add("d-none");
+  makeYourChoice.classList.add("d-none");
 });
 
 beFreelancerButton.addEventListener("click", function () {
   signupForm.classList.remove("d-none");
   hireFreelancerButton.classList.add("d-none");
   beFreelancerButton.classList.add("d-none");
+  makeYourChoice.classList.add("d-none");
 });
 
 signupCancelButton.addEventListener("click", function() {
   signupForm.classList.add("d-none");
   hireFreelancerButton.classList.remove("d-none");
   beFreelancerButton.classList.remove("d-none");
+  makeYourChoice.classList.remove("d-none");
 });
