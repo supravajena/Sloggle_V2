379c7298840b6970d9a336edc45ef58dab9e8a2c
Suprava Jena <supravajena48@gmail.com>
2021-02-26 01:14:12 +0530

    Updates JS file for modal view triggers

--- SloggleUI/static/SloggleUI/main.js
+++ SloggleUI/static/SloggleUI/main.js
@@ -5,3 +5,28 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+// for signup modal
+
+const signupForm = document.getElementById("signupForm");
+const hireFreelancerButton = document.getElementById("hireFreelancerButton");
+const beFreelancerButton = document.getElementById("beFreelancerButton");
+const signupCancelButton = document.getElementById("signupCancelButton");
+
+hireFreelancerButton.addEventListener("click", function () {
+  signupForm.classList.remove("d-none");
+  hireFreelancerButton.classList.add("d-none");
+  beFreelancerButton.classList.add("d-none");
+});
+
+beFreelancerButton.addEventListener("click", function () {
+  signupForm.classList.remove("d-none");
+  hireFreelancerButton.classList.add("d-none");
+  beFreelancerButton.classList.add("d-none");
+});
+
+signupCancelButton.addEventListener("click", function() {
+  signupForm.classList.add("d-none");
+  hireFreelancerButton.classList.remove("d-none");
+  beFreelancerButton.classList.remove("d-none");
+});
